88ed491ee0942abb642cc3caed1900dd336efc09
milad_ebp <ebrahimpoumilad74@gmail.com>
2019-12-12 19:29:16 +0330

    #gitegnore

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    server: {
+        host: 'localhost',
+        port: 8000
+    },
+    database: {
+        host: '127.0.0.1',
+        port: 27017,
+        mainDb: 'hapi',
+        username: '',
+        password: ''
+    }
+};
+//# sourceMappingURL=config.js.map
